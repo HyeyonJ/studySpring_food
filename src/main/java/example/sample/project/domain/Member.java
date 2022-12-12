efb81202f218b56da976ef4b410dfb7b079b3290
HyeyeonJ <michaelvia29@gmail.com>
2022-12-12 15:07:53 +0900

    DB 연결 설정 추가

--- src/main/java/example/sample/project/domain/Member.java
+++ src/main/java/example/sample/project/domain/Member.java
@@ -1,8 +1,12 @@
 package example.sample.project.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 	private Integer id;
 	private String loginId;
---
+++ src/main/java/example/sample/project/repository/mybatis/MemberMapper.java
@@ -0,0 +1,20 @@
+package example.sample.project.repository.mybatis;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import example.sample.project.domain.Member;
+
+@Mapper
+public interface MemberMapper {
+	public Member insert(Member member);
+	
+	public Member selectById(int id);
+	
+	public Member selectByLoginId(String loginId);
+	
+	public List<Member> selectAll();
+	
+	public void deleteAll();
+}
